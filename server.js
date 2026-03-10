bd7c8dfb0931b2c69af3151b3ed1660604ec3795
Manoj-Khadka <khadkamanoj900@gmail.com>
2026-03-10 21:25:29 +0545

    Dynamic page  not found

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const path = require("path");
 
 const app = express();
 
@@ -11,8 +12,11 @@ app.use(bodyParser.urlencoded());
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 app.use(shopRoutes);
+
 app.use((req, res, next) => {
-  res.status(404).send("<h1>Page not found</h1>");
+  res
+    .status(404)
+    .sendFile(path.join(__dirname, "views", "page-not-found.html"));
 });
 
 app.listen(3000);

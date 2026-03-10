df7208700b5c822e77156c8a28e2bc2211f25da3
Manoj-Khadka <khadkamanoj900@gmail.com>
2026-03-10 21:18:48 +0545

    Static HTML Pages

--- routes/admin.js
+++ routes/admin.js
@@ -1,11 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const path = require("path");
 
 router.get("/add-product", (req, res, next) => {
-  console.log(" second middleware");
-  res.send(
-    '<form action="/admin/add-product" method = "POST"> <input type="text" name = "Title"> <button type="submit">Add</button></form>',
-  );
+  res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
 });
 
 router.post("/add-product", (req, res, next) => {
--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const path = require("path");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  console.log("===>", req.body);
-  res.send("Hello from express");
+  // res.sendFile("/views/shop.html");
+  // Here / refers to the root  folder on our OS not to the Project folder
+  res.sendFile(path.join(__dirname, "../", "views", "shop.html"));
 });
 
 module.exports = router;

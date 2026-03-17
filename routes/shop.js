527ab2408c65e07bb32c3fdb721199afb83feef6
Manoj-Khadka <khadkamanoj900@gmail.com>
2026-03-17 17:46:50 +0545

    VIEWS

--- routes/shop.js
+++ routes/shop.js
@@ -2,10 +2,12 @@ const express = require("express");
 const path = require("path");
 const router = express.Router();
 
+const rootDir = require("../dir/path");
+
 router.get("/", (req, res, next) => {
   // res.sendFile("/views/shop.html");
   // Here / refers to the root  folder on our OS not to the Project folder
-  res.sendFile(path.join(__dirname, "../", "views", "shop.html"));
+  res.sendFile(path.join(rootDir, "views", "shop.html"));
 });
 
 module.exports = router;

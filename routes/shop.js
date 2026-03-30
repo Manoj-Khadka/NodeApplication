151eadc695a2b6121361a902a4b9e37d2da302ef
Manoj-Khadka <khadkamanoj900@gmail.com>
2026-03-30 19:52:57 +0545

    Controller

--- routes/shop.js
+++ routes/shop.js
@@ -4,7 +4,6 @@ const express = require("express");
 
 const productsController = require("../controllers/products");
 const router = express.Router();
-const adminData = require("./admin");
 router.get("/", productsController.getProducts);
 
 module.exports = router;

f4fecd0e5cd28cb8d667fc658581da06a4c2005e
Manoj-Khadka <khadkamanoj900@gmail.com>
2026-04-08 18:15:19 +0545

    REST API

---
+++ REST/routes/feed.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const feedController = require("../Controllers/feed");
+const router = express.Router();
+
+// GET
+router.get("/posts", feedController.getPosts);
+
+// POST
+router.post("/posts", feedController.createPost);
+
+module.exports = router;
